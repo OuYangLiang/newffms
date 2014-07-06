0f64d5c2612335923558fae3e3df5d441e5abd76
OuYangLiang <ouyanggod@gmail.com>
2014-07-06 22:45:40 +0800

    Encrypt password with SHA2

--- src/main/java/com/personal/oyl/newffms/security/MyUserDetailService.java
+++ src/main/java/com/personal/oyl/newffms/security/MyUserDetailService.java
@@ -15,10 +15,10 @@ public class MyUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
         
-        if (username.equals("oyl")) {
+        if (username.equalsIgnoreCase("oyl")) {
             AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
             
-            return new User(username, "password123", AUTHORITIES);
+            return new User(username, "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2", AUTHORITIES);
         }
         
         throw new UsernameNotFoundException(username);
